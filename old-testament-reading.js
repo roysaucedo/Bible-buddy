4edddf63ab7d880a391fa7bf58048dabc0e1c31d
roysaucedo <roy.a.saucedo@gmail.com>
2023-04-27 09:22:07 -0700

    OT

---
+++ old-testament-reading.js
@@ -0,0 +1,76 @@
+const readingsContainer = document.getElementById("readingsContainer");
+
+// Fetch the JSON data
+fetch("/json-data/old-testament/old-testament-data.json")
+  .then((response) => response.json())
+  .then((data) => {
+    // Loop through the months in the JSON data
+    for(let i=0; i<data.length; i++){
+        const monthData = data[i];
+        const monthHeader = document.createElement("h2");
+        monthHeader.textContent = monthData.month;
+        
+
+        readingsContainer.appendChild(monthHeader);
+
+        const table = document.createElement("table");
+        const tableHeader = document.createElement("tr");
+        const dayHeader = document.createElement("th");
+        dayHeader.textContent = "Day";
+        const readingHeader = document.createElement("th");
+        readingHeader.textContent = "Reading";
+        tableHeader.appendChild(dayHeader);
+        tableHeader.appendChild(readingHeader);
+        table.appendChild(tableHeader);
+
+        
+        
+
+        for(let j=0; j<monthData.day.length; j++){
+            const row = document.createElement("tr");
+            const dayCell = document.createElement("td");
+            dayCell.textContent = monthData.day[j];
+            const readingCell = document.createElement("td");
+            readingCell.textContent = monthData.reading[j];
+            row.appendChild(dayCell);
+            row.appendChild(readingCell);
+            table.appendChild(row);
+
+            // STYLES
+            row.classList.add("bg-gray-50","p-4", "border-2");
+            dayCell.classList.add("text-xl", "p-2","border-2");
+            readingCell.classList.add("text-xl", "p-2","border-2");
+        }
+
+        // STYLES
+        monthHeader.classList.add("text-4xl", "text-center", "font-semibold", "mb-6", "md:text-5xl")
+        table.classList.add("w-full","mb-20","mx-auto");
+        tableHeader.classList.add();
+        dayHeader.classList.add("text-xl", "p-2","border-2","bg-gray-200", "md:text-2xl");
+        readingHeader.classList.add("text-xl", "p-2","border-2","bg-gray-200","md:text-2xl");
+
+
+        readingsContainer.appendChild(table);
+    }
+    
+  })
+  .catch((error) => {
+    console.error("Error fetching JSON data: ", error);
+  });
+
+
+
+
+
+//   monthElement.textContent = data.month;
+
+//     const days = data.day;
+//     const readings = data.reading;
+
+//     for (let i = 0; i < days.length; i++) {
+//       const day = days[i];
+//       const reading = readings[i];
+//       const div = document.createElement("div");
+//       div.textContent = `${day}: ${reading}`;
+//       readingsContainer.appendChild(div);
+//     }
